87ceed9c6ba131970fb919a90bc8e51749236799
happenwah <tiagolixa@hotmail.com>
2021-12-29 17:22:17 +0000

    feat: self-paying tx logic

--- contracts/GelatoRelayer.sol
+++ contracts/GelatoRelayer.sol
@@ -2,23 +2,27 @@
 pragma solidity 0.8.11;
 
 import {GelatoBytes} from "./libraries/GelatoBytes.sol";
+import {GelatoString} from "./libraries/GelatoString.sol";
+import {getBalance} from "./functions/TokenUtils.sol";
+import {IOracleAggregator} from "./interfaces/IOracleAggregator.sol";
+import {NATIVE_TOKEN} from "./constants/Tokens.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract GelatoRelayer {
     using GelatoBytes for bytes;
+    using GelatoString for string;
 
     struct Request {
         address from;
-        address to;
-        uint256 value;
+        address[] targets;
         uint256 gasLimit;
         uint256 relayerNonce;
         uint256 chainId;
         uint256 deadline;
         address paymentToken;
-        bool isToEIP2771Compliant;
+        bool[] isTargetEIP2771Compliant;
         bool isFlashbotsTx;
-        bytes payload;
+        bytes[] payloads;
     }
 
     bytes32 public constant REQUEST_TYPEHASH =
@@ -31,14 +35,28 @@ contract GelatoRelayer {
     // solhint-disable-next-line max-line-length
     string public constant EIP712_DOMAIN_TYPE =
         "EIP712Domain(string name,string version,address verifyingContract,bytes32 salt)";
+    uint256 public constant DIAMOND_CALL_OVERHEAD = 21000;
 
     address public immutable gelato;
     uint256 public immutable chainId;
     bytes32 public immutable domainSeparator;
+    IOracleAggregator public immutable oracleAggregator;
 
+    uint256 public relayFeePct;
     mapping(address => uint256) private _relayerNonces;
+    mapping(address => mapping(address => uint256)) private _userTokenBalances;
 
-    constructor(address _gelato) {
+    modifier onlyGelato() {
+        require(msg.sender == gelato, "Only callable by gelato");
+        _;
+    }
+
+    constructor(
+        address _gelato,
+        string memory version,
+        address _oracleAggregator,
+        uint256 _relayFeePct
+    ) {
         gelato = _gelato;
         uint256 _chainId;
         // solhint-disable-next-line no-inline-assembly
@@ -50,38 +68,66 @@ contract GelatoRelayer {
             abi.encode(
                 keccak256(bytes(EIP712_DOMAIN_TYPE)),
                 keccak256(bytes("GelatoRelayer")),
-                keccak256(bytes("0.1")),
+                keccak256(bytes(version)),
                 address(this),
                 bytes32(chainId)
             )
         );
+        oracleAggregator = IOracleAggregator(_oracleAggregator);
+        relayFeePct = _relayFeePct;
     }
 
-    function execute(Request calldata req, bytes calldata signature)
+    function executeSelfPayingTx(Request calldata req, bytes calldata signature)
         external
-        payable
+        onlyGelato
     {
-        require(msg.sender == gelato, "GelatoRelayer.execute: Only gelato");
-        require(
-            req.relayerNonce == getRelayerNonce(req.from),
-            "GelatoRelayer.execute: Invalid relayer nonce"
-        );
-        _relayerNonces[req.from] += 1;
-        _verifySignature(req, signature);
-        require(
-            req.chainId == chainId,
-            "GelatoRelayer.execute: Invalid chainId"
-        );
+        uint256 startGas = gasleft();
         require(
             // solhint-disable-next-line not-rely-on-time
             req.deadline == 0 || block.timestamp <= req.deadline,
             "GelatoRelayer.execute: Expired"
         );
-        (bool success, bytes memory returnData) = req.to.call{
-            gas: req.gasLimit,
-            value: req.value
-        }(req.payload);
-        if (!success) returnData.revertWithError("GelatoRelayer.execute:");
+        _verifyAndIncrementNonce(req.relayerNonce, req.from);
+        _verifySignature(req, signature);
+        require(
+            req.chainId == chainId,
+            "GelatoRelayer.execute: Invalid chainId"
+        );
+        uint256 preBalance = getBalance(req.paymentToken, address(this));
+        _multiCall(
+            req.from,
+            req.targets,
+            req.isTargetEIP2771Compliant,
+            req.payloads
+        );
+        uint256 postBalance = getBalance(req.paymentToken, address(this));
+        require(
+            postBalance >= preBalance,
+            "GelatoRelayer.execute: paymentToken balance decreased"
+        );
+        uint256 credit;
+        unchecked {
+            credit = postBalance - preBalance;
+        }
+        uint256 gasCost = startGas + DIAMOND_CALL_OVERHEAD - gasleft();
+        require(
+            gasCost <= req.gasLimit,
+            "GelatoRelayer.execute: gasLimit exceeded"
+        );
+        uint256 gasDebitInNativeToken = (gasCost *
+            tx.gasprice *
+            (100 + relayFeePct)) / 100;
+        uint256 gasDebitInCreditToken = req.paymentToken == NATIVE_TOKEN
+            ? gasDebitInNativeToken
+            : _getGasDebitInCreditToken(
+                credit,
+                req.paymentToken,
+                gasDebitInNativeToken
+            );
+        require(
+            credit >= gasDebitInCreditToken,
+            "GelatoRelayer.execute: Insufficient payment"
+        );
     }
 
     function getRelayerNonce(address _from)
@@ -90,6 +136,63 @@ contract GelatoRelayer {
         returns (uint256 relayerNonce)
     {
         relayerNonce = _relayerNonces[_from];
+    }
+
+    function _verifyAndIncrementNonce(uint256 _relayerNonce, address _from)
+        private
+    {
+        require(
+            _relayerNonce == getRelayerNonce(_from),
+            "GelatoRelayer.execute: Invalid relayer nonce"
+        );
+        _relayerNonces[_from] += 1;
+    }
+
+    function _multiCall(
+        address _from,
+        address[] calldata _targets,
+        bool[] calldata _isTargetEIP2771Compliant,
+        bytes[] calldata _payloads
+    ) private {
+        require(
+            _targets.length == _payloads.length &&
+                _targets.length == _isTargetEIP2771Compliant.length,
+            "GelatoRelayer.execute: Array length mismatch"
+        );
+        for (uint256 i; i < _targets.length; i++) {
+            (bool success, bytes memory returnData) = _targets[i].call(
+                _isTargetEIP2771Compliant[i]
+                    ? abi.encodePacked(_payloads[i], _from)
+                    : _payloads[i]
+            );
+            if (!success) returnData.revertWithError("GelatoRelayer.execute:");
+        }
+    }
+
+    function _getGasDebitInCreditToken(
+        uint256 _credit,
+        address _creditToken,
+        uint256 _gasDebitInNativeToken
+    ) private view returns (uint256 gasDebitInCreditToken) {
+        if (_credit == 0) return 0;
+
+        try
+            oracleAggregator.getExpectedReturnAmount(
+                _gasDebitInNativeToken,
+                NATIVE_TOKEN,
+                _creditToken
+            )
+        returns (uint256 gasDebitInCreditToken_, uint256) {
+            require(
+                gasDebitInCreditToken_ != 0,
+                "ExecFacet.exec:  _creditToken not on OracleAggregator"
+            );
+            gasDebitInCreditToken = gasDebitInCreditToken_;
+        } catch Error(string memory err) {
+            err.revertWithInfo("ExecFacet.exec: OracleAggregator:");
+        } catch {
+            revert("ExecFacet.exec: OracleAggregator: unknown error");
+        }
     }
 
     function _verifySignature(Request calldata req, bytes calldata signature)
@@ -118,15 +221,14 @@ contract GelatoRelayer {
         encodedReq = abi.encode(
             REQUEST_TYPEHASH,
             req.from,
-            req.to,
-            req.value,
+            req.targets,
             req.gasLimit,
             req.relayerNonce,
             req.chainId,
             req.deadline,
-            req.isToEIP2771Compliant,
+            req.isTargetEIP2771Compliant,
             req.isFlashbotsTx,
-            keccak256(req.payload)
+            keccak256(abi.encode(req.payloads))
         );
     }
 }
