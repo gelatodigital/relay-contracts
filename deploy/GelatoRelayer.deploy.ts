38a456bad3b4ca8275f24195ec71494097b54f29
happenwah <tiagolixa@hotmail.com>
2022-03-04 18:41:25 +0000

    chore: goerli deployment

--- deploy/GelatoRelayer.deploy.ts
+++ deploy/GelatoRelayer.deploy.ts
@@ -7,45 +7,31 @@ import { getAddresses } from "../src/addresses";
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   if (hre.network.name === "mainnet" || hre.network.name === "goerli") {
     console.log(
-      `Deploying GelatoRelayer to ${hre.network.name}. Hit ctrl + c to abort`
+      `Deploying GelatoMetaBox to ${hre.network.name}. Hit ctrl + c to abort`
     );
     await sleep(10000);
   }
 
   const { deploy } = deployments;
-  const { deployer, gelatoMultiSig } = await getNamedAccounts();
+  const { deployer } = await getNamedAccounts();
   const addresses = getAddresses(hre.network.name);
-  const treasuryAddress = (await deployments.get("Treasury")).address;
-  const version = "0.1";
 
-  await deploy("GelatoRelayer", {
+  await deploy("GelatoMetaBox", {
     from: deployer,
     proxy: {
-      owner: gelatoMultiSig,
+      owner: deployer,
       proxyContract: "EIP173Proxy",
-      execute: {
-        init: {
-          methodName: "initialize",
-          args: [],
-        },
-      },
     },
-    args: [
-      addresses.Gelato,
-      addresses.OracleAggregator,
-      treasuryAddress,
-      version,
-    ],
+    args: [addresses.Gelato],
     log: hre.network.name != "hardhat" ? true : false,
   });
 };
 
 export default func;
 
-func.skip = async (hre: HardhatRuntimeEnvironment) => {
+/*func.skip = async (hre: HardhatRuntimeEnvironment) => {
   const shouldSkip =
     hre.network.name === "mainnet" || hre.network.name === "goerli";
   return shouldSkip ? true : false;
-};
-func.tags = ["GelatoRelayer"];
-func.dependencies = ["Treasury"];
+};*/
+func.tags = ["GelatoMetaBox"];
