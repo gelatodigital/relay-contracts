6d4712bde7817bba7d94dc5ee53bf028f3f9896b
Sasha <sasha@gelato.digital>
2022-07-14 11:27:17 +0200

    GN-200: Merge 4 contracts into 2, GelatoRelay.sol and GelatoRelayPullFee.sol

--- contracts/GelatoRelay.sol
+++ contracts/GelatoRelay.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import {ForwardRequest} from "./structs/RequestTypes.sol";
-import {GelatoRelayForwarderBase} from "./base/GelatoRelayForwarderBase.sol";
 import {GelatoCallUtils} from "./gelato/GelatoCallUtils.sol";
+import {GelatoRelayBase} from "./base/GelatoRelayBase.sol";
 import {GelatoTokenUtils} from "./gelato/GelatoTokenUtils.sol";
+import {ForwardRequest} from "./structs/RequestTypes.sol";
+import {IGelato} from "./interfaces/IGelato.sol";
+import {MetaTxRequest} from "./structs/RequestTypes.sol";
 import {Proxied} from "./vendor/hardhat-deploy/Proxied.sol";
 import {
     Initializable
 } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-/// @title Gelato Relay Forwarder contract
+/// @title Gelato Meta Box contract
 /// @notice This contract must NEVER hold funds!
 /// @dev    Maliciously crafted transaction payloads could wipe out any funds left here.
 // solhint-disable-next-line max-states-count
-contract GelatoRelayForwarder is
-    Proxied,
-    Initializable,
-    GelatoRelayForwarderBase
-{
+contract GelatoRelay is Proxied, Initializable, GelatoRelayBase {
+    // have to merge the base files as well
     address public immutable gelato;
     uint256 public immutable chainId;
 
@@ -49,6 +48,15 @@ contract GelatoRelayForwarder is
         address feeToken,
         uint256 fee,
         bytes32 taskId
+    );
+    event LogMetaTxRequestAsyncGasTankFee(
+        bytes32 indexed taskId,
+        address indexed user
+    );
+
+    event LogMetaTxRequestSyncGasTankFee(
+        bytes32 indexed taskId,
+        address indexed user
     );
 
     event LogSetGasTank(address oldGasTank, address newGasTank);
@@ -108,6 +116,12 @@ contract GelatoRelayForwarder is
         gasTankAdmin = _gasTankAdmin;
     }
 
+    /// @notice Relay request + Sync Payment (target pays Gelato during call forward)
+    /// @param _target Target smart contract
+    /// @param _data Payload for call on _target
+    /// @param _gas Gas limit
+    /// @param _gelatoFee Fee to be charged, denominated in feeToken
+    /// @param _taskId Unique task indentifier
     function forwardCallSyncFee(
         address _target,
         bytes calldata _data,
@@ -135,6 +149,14 @@ contract GelatoRelayForwarder is
         emit LogForwardCallSyncFee(_target, _feeToken, amount, _taskId);
     }
 
+    /// @notice Relay request + async Gas Tank payment deductions (off-chain accounting)
+    /// @param _req Relay request data
+    /// @param _sponsorSignature EIP-712 compliant signature from _req.sponsor
+    /// @param _gelatoFee Fee to be charged by Gelato relayer, denominated in _req.feeToken
+    /// @notice Oracle value for exchange rate between native tokens and fee token
+    /// @param  _nativeToFeeTokenXRateNumerator Exchange rate numerator
+    /// @param  _nativeToFeeTokenXRateNumerator Exchange rate numerator
+    /// @param _taskId Unique task indentifier
     // solhint-disable-next-line function-max-lines
     function forwardRequestGasTankFee(
         ForwardRequest calldata _req,
@@ -228,7 +250,77 @@ contract GelatoRelayForwarder is
         }
     }
 
-    function getDomainSeparator() public view returns (bytes32) {
+    /// @notice Relay request + async Gas Tank payment deductions (off-chain accounting)
+    /// @param _req Relay request data
+    /// @param _userSignature EIP-712 compliant signature from _req.user
+    /// @param _sponsorSignature EIP-712 compliant signature from _req.sponsor
+    ///                          (can be same as _userSignature)
+    /// @notice   EOA that originates the tx, but does not necessarily pay the relayer
+    /// @param _gelatoFee Fee to be charged by Gelato relayer, denominated in _req.feeToken
+    // solhint-disable-next-line function-max-lines
+    function metaTxRequestGasTankFee(
+        MetaTxRequest calldata _req,
+        bytes calldata _userSignature,
+        bytes calldata _sponsorSignature,
+        uint256 _gelatoFee,
+        uint256 _nativeToFeeTokenXRateNumerator,
+        uint256 _nativeToFeeTokenXRateDenominator,
+        bytes32 _taskId
+    ) external onlyGelato {
+        require(
+            // solhint-disable-next-line not-rely-on-time
+            _req.deadline == 0 || _req.deadline >= block.timestamp,
+            "Request expired"
+        );
+
+        require(_req.chainId == chainId, "Wrong chainId");
+
+        require(
+            _req.paymentType == 1 || _req.paymentType == 2,
+            "paymentType must be 1 or 2"
+        );
+
+        require(_gelatoFee <= _req.maxFee, "Executor over-charged");
+
+        // Verify and increment user's nonce
+        uint256 userNonce = nonce[_req.user];
+        require(_req.nonce == userNonce, "Invalid nonce");
+        nonce[_req.user] = userNonce + 1;
+
+        _verifyMetaTxRequestSignature(_req, _userSignature, _req.user);
+        // If is sponsored tx, we also verify sponsor's signature
+        if (_req.user != _req.sponsor) {
+            _verifyMetaTxRequestSignature(
+                _req,
+                _sponsorSignature,
+                _req.sponsor
+            );
+        }
+
+        require(_req.target != gasTank, "target address cannot be gasTank");
+        require(_isContract(_req.target), "Cannot call EOA");
+        (bool success, ) = _req.target.call{gas: _req.gas}(
+            abi.encodePacked(_req.data, _req.user)
+        );
+        require(success, "External call failed");
+
+        if (_req.paymentType == 1) {
+            emit LogMetaTxRequestAsyncGasTankFee(_taskId, _req.user);
+
+            emit LogUseGelato1Balance(
+                _req.sponsor,
+                address(this),
+                _req.feeToken,
+                chainId,
+                _nativeToFeeTokenXRateNumerator,
+                _nativeToFeeTokenXRateDenominator
+            );
+        } else if (_req.paymentType == 2) {
+            emit LogMetaTxRequestSyncGasTankFee(_taskId, _req.user);
+        }
+    }
+
+    function getDomainSeparator() external view returns (bytes32) {
         return _getDomainSeparator(chainId);
     }
 }
--- contracts/GelatoRelayPullFee.sol
+++ contracts/GelatoRelayPullFee.sol
@@ -2,30 +2,23 @@
 pragma solidity 0.8.15;
 
 import {ForwardRequest} from "./structs/RequestTypes.sol";
+import {MetaTxRequest} from "./structs/RequestTypes.sol";
 import {NATIVE_TOKEN} from "./constants/Tokens.sol";
-import {GelatoRelayForwarderBase} from "./base/GelatoRelayForwarderBase.sol";
+import {GelatoRelayBase} from "./base/GelatoRelayBase.sol";
 import {GelatoCallUtils} from "./gelato/GelatoCallUtils.sol";
 import {IGelato} from "./interfaces/IGelato.sol";
 import {IGelatoRelayAllowances} from "./interfaces/IGelatoRelayAllowances.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {
     SafeERC20
 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {
     EnumerableSet
 } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-/// @title Gelato Relay Forwarder Pull Fee contract
-/// @notice Forward calls + fee payment with transferFrom sponsor's address to gelato
-/// @dev    Maliciously crafted transaction payloads could wipe out any funds left here.
-// solhint-disable-next-line max-states-count
-contract GelatoRelayForwarderPullFee is
-    GelatoRelayForwarderBase,
-    Ownable,
-    Pausable
-{
+contract GelatoRelayPullFee is GelatoRelayBase, Ownable, Pausable {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     address public immutable gelato;
@@ -38,6 +31,15 @@ contract GelatoRelayForwarderPullFee is
 
     event LogForwardRequestPullFee(
         address indexed sponsor,
+        address indexed target,
+        address feeToken,
+        uint256 fee,
+        bytes32 taskId
+    );
+
+    event LogMetaTxRequestPullFee(
+        address indexed sponsor,
+        address indexed user,
         address indexed target,
         address feeToken,
         uint256 fee,
@@ -88,6 +90,14 @@ contract GelatoRelayForwarderPullFee is
         _whitelistedDest.remove(_dest);
     }
 
+    /// @notice Relay forward request + pull fee from (transferFrom) _req.sponsor's address
+    /// @dev    Assumes that _req.sponsor has approved this contract to spend _req.feeToken
+    /// @param _req Relay request data
+    /// @param _sponsorSignature EIP-712 compliant signature from _req.sponsor
+    ///                          (can be same as _userSignature)
+    /// @notice   EOA that originates the tx, but does not necessarily pay the relayer
+    /// @param _gelatoFee Fee to be charged by Gelato relayer, denominated in _req.feeToken
+    /// @param _taskId Gelato task id
     // solhint-disable-next-line function-max-lines
     function forwardRequestPullFee(
         ForwardRequest calldata _req,
@@ -167,6 +177,90 @@ contract GelatoRelayForwarderPullFee is
 
         emit LogForwardRequestPullFee(
             _req.sponsor,
+            _req.target,
+            _req.feeToken,
+            _gelatoFee,
+            _taskId
+        );
+    }
+
+    /// @notice Relay meta tx request + pull fee from (transferFrom) _req.sponsor's address
+    /// @dev    Assumes that _req.sponsor has approved this contract to spend _req.feeToken
+    /// @param _req Relay request data
+    /// @param _userSignature EIP-712 compliant signature from _req.user
+    /// @param _sponsorSignature EIP-712 compliant signature from _req.sponsor
+    ///                          (can be same as _userSignature)
+    /// @notice   EOA that originates the tx, but does not necessarily pay the relayer
+    /// @param _gelatoFee Fee to be charged by Gelato relayer, denominated in _req.feeToken
+    /// @notice Handles the case of tokens with fee on transfer
+    /// @param _taskId Gelato task id
+    // solhint-disable-next-line function-max-lines
+    function metaTxRequestPullFee(
+        MetaTxRequest calldata _req,
+        bytes calldata _userSignature,
+        bytes calldata _sponsorSignature,
+        uint256 _gelatoFee,
+        bytes32 _taskId
+    ) external onlyGelato whenNotPaused {
+        require(
+            // solhint-disable-next-line not-rely-on-time
+            _req.deadline == 0 || _req.deadline >= block.timestamp,
+            "Request expired"
+        );
+
+        require(_req.chainId == chainId, "Wrong chainId");
+
+        require(_req.paymentType == 3, "paymentType must be 3");
+
+        require(
+            _whitelistedDest.contains(_req.target),
+            "target address not whitelisted"
+        );
+
+        require(
+            _req.feeToken != NATIVE_TOKEN,
+            "Native token not supported for paymentType 3"
+        );
+
+        require(_gelatoFee <= _req.maxFee, "Executor over-charged");
+
+        // Verify and increment user's nonce
+        uint256 userNonce = nonce[_req.user];
+        require(_req.nonce == userNonce, "Invalid nonce");
+        nonce[_req.user] = userNonce + 1;
+
+        _verifyMetaTxRequestSignature(_req, _userSignature, _req.user);
+        // If is sponsored tx, we also verify sponsor's signature
+        if (_req.user != _req.sponsor) {
+            _verifyMetaTxRequestSignature(
+                _req,
+                _sponsorSignature,
+                _req.sponsor
+            );
+        }
+        // Gas optimization
+        address pullFeeRegistryCopy = pullFeeRegistry;
+        {
+            require(
+                _req.target != pullFeeRegistryCopy,
+                "Unsafe call to pullFeeRegistry"
+            );
+            require(_isContract(_req.target), "Cannot call EOA");
+            (bool success, ) = _req.target.call{gas: _req.gas}(
+                abi.encodePacked(_req.data, _req.user)
+            );
+            require(success, "External call failed");
+        }
+
+        IGelatoRelayAllowances(pullFeeRegistryCopy).pullFeeFrom(
+            _req.feeToken,
+            _req.sponsor,
+            _gelatoFee
+        );
+
+        emit LogMetaTxRequestPullFee(
+            _req.sponsor,
+            _req.user,
             _req.target,
             _req.feeToken,
             _gelatoFee,
