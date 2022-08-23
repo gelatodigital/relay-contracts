d50dae6c2ef08d8fa070c8321fabd717e37428b5
Luis Schliesske <36712489+gitpusha@users.noreply.github.com>
2022-08-23 17:14:48 +0200

    refactor: audit new GelatoRelay sys, deprecate MetaBox (#18)
    Co-authored-by: Sasha <sasha@gelato.digital>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,8 +11,7 @@ import * as dotenv from "dotenv";
 import { ethers } from "ethers";
 dotenv.config({ path: __dirname + "/.env" });
 
-const DEPLOYER_PK = process.env.DEPLOYER_PK;
-const DEPLOYER_PK_MAINNET = process.env.DEPLOYER_PK_MAINNET;
+const RELAY_DEPLOYER_PK = process.env.RELAY_DEPLOYER_PK;
 const ALCHEMY_ID = process.env.ALCHEMY_ID;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
@@ -24,82 +23,85 @@ const config: HardhatUserConfig = {
     deployer: {
       default: 0,
     },
+    relayDeployer: {
+      default: "0xd1Ac051Dc0E1366502eF3Fe4D754fbeC6986a177",
+    },
   },
 
   networks: {
     hardhat: {
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_ID}`,
-        blockNumber: 13476568, // ether price $4,168.96
+        blockNumber: 13476568,
       },
       accounts: {
         accountsBalance: ethers.utils.parseEther("10000").toString(),
       },
     },
     alfajores: {
-      accounts: DEPLOYER_PK_MAINNET ? [DEPLOYER_PK_MAINNET] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 44787,
       url: "https://alfajores-forno.celo-testnet.org",
     },
     avalanche: {
       url: "https://api.avax.network/ext/bc/C/rpc",
       chainId: 43114,
-      accounts: DEPLOYER_PK ? [DEPLOYER_PK] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
     },
     bsc: {
-      accounts: DEPLOYER_PK_MAINNET ? [DEPLOYER_PK_MAINNET] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 56,
       url: "https://bsc-dataseed1.ninicoin.io/",
     },
     evmos: {
-      accounts: DEPLOYER_PK_MAINNET ? [DEPLOYER_PK_MAINNET] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 9001,
       url: "https://eth.bd.evmos.org:8545",
     },
     rinkeby: {
-      accounts: DEPLOYER_PK ? [DEPLOYER_PK] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 4,
       url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_ID}`,
     },
     gnosis: {
-      accounts: DEPLOYER_PK_MAINNET ? [DEPLOYER_PK_MAINNET] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 100,
       url: `https://rpc.gnosischain.com/`,
     },
     goerli: {
-      accounts: DEPLOYER_PK ? [DEPLOYER_PK] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 5,
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_ID}`,
     },
     kovan: {
-      accounts: DEPLOYER_PK ? [DEPLOYER_PK] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 42,
       url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_ID}`,
     },
     mainnet: {
-      accounts: DEPLOYER_PK_MAINNET ? [DEPLOYER_PK_MAINNET] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 1,
       url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_ID}`,
     },
     matic: {
-      accounts: DEPLOYER_PK_MAINNET ? [DEPLOYER_PK_MAINNET] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 137,
       url: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_ID}`,
     },
     mumbai: {
-      accounts: DEPLOYER_PK ? [DEPLOYER_PK] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
       chainId: 80001,
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_ID}`,
     },
     moonbeam: {
       url: "https://moonbeam.api.onfinality.io/public",
       chainId: 1284,
-      accounts: DEPLOYER_PK ? [DEPLOYER_PK] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
     },
     moonriver: {
       url: "https://moonriver-rpc.dwellir.com",
       chainId: 1285,
-      accounts: DEPLOYER_PK ? [DEPLOYER_PK] : [],
+      accounts: RELAY_DEPLOYER_PK ? [RELAY_DEPLOYER_PK] : [],
     },
   },
   verify: {
@@ -111,27 +113,9 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.13",
+        version: "0.8.16",
         settings: {
-          optimizer: { enabled: true, runs: 200000 },
-          modelChecker: {
-            targets: [
-              "balance",
-              "outOfBounds",
-              "popEmptyArray",
-              "constantCondition",
-              "divByZero",
-              "assert",
-              "underflow",
-              "overflow",
-            ],
-            showUnproved: true,
-            engine: "none",
-            // contracts: {
-            //   "contracts/GelatoRelayer.sol": ["GelatoRelayer"],
-            // },
-            // invariants: ["contract", "reentrancy"],
-          },
+          optimizer: { enabled: true, runs: 999999 },
         },
       },
     ],
