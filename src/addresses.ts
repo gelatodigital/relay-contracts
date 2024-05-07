fdf2995dd10e5b01f7795b069f1b60da2b792161
Ogulcan Patavi <pataviogulcan@gmail.com>
2024-05-07 16:43:16 +0300

    Chore/gn 4849 connext sepolia (#92)
    chore: connextSepolia

--- src/addresses.ts
+++ src/addresses.ts
@@ -74,6 +74,10 @@ export const getAddresses = (network: string): Addresses => {
       return {
         GELATO: "0xF82D64357D9120a760e1E4C75f646C0618eFc2F3",
       };
+    case "connextSepolia":
+      return {
+        GELATO: "0x30056FD86993624B72c7400bB4D7b29F05928E59",
+      };
     case "ethereum":
       return {
         GELATO: "0x3CACa7b48D0573D793d3b0279b5F0029180E83b6",
